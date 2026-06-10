7ab30569dde7265f50cc9758fc36ff8eb7019247
Amber <dengreamber155jhs606@gmail.com>
2026-06-10 10:28:35 +0530

    Day-5 uploaded.

---
+++ Day5/largestprime.c
@@ -0,0 +1,18 @@
+//Write a program to Find largest prime factor.
+#include <stdio.h>
+int main()
+{
+    int n,i,largest=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            largest=i;
+            n=n/i;
+        }
+    }
+    printf("Largest prime factor of the given number is: %d", largest);
+    return 0;
+}
